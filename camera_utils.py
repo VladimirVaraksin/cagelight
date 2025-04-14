a73a94118dbbc92ec364c456ad7d60b8186c643a
vlavar <145839850+VladimirVaraksin@users.noreply.github.com>
2025-04-14 17:03:47 +0200

    c

--- camera_utils.py
+++ camera_utils.py
@@ -2,6 +2,10 @@ import AVFoundation
 import cv2
 from object_detection import model, detect_obj
 
+
+
+#macOS solution
+#"""
 def get_cameras_with_resolutions():
     devices = AVFoundation.AVCaptureDevice.devicesWithMediaType_(AVFoundation.AVMediaTypeVideo)
     camera_data = []
@@ -27,8 +31,50 @@ def get_cameras_with_resolutions():
         })
 
     return camera_data
+#"""
+#Platform independent solution
+"""
+def get_cameras_with_resolutions(max_tested_devices=5):
+    common_resolutions = [
+        (1920, 1080), (1280, 720), (1024, 768),
+        (800, 600), (640, 480), (320, 240)
+    ]
+
+    camera_data = []
+
+    for index in range(max_tested_devices):
+
+            cap = cv2.VideoCapture(index)
+
+            # If the camera couldn't be opened, it will raise an exception
+            if not cap.isOpened():
+                print(f"Camera at index {index} could not be opened.")
+                return camera_data
 
 
+            name = f"Camera {index}"
+            supported_res = []
+
+            for width, height in common_resolutions:
+                cap.set(cv2.CAP_PROP_FRAME_WIDTH, width)
+                cap.set(cv2.CAP_PROP_FRAME_HEIGHT, height)
+
+                actual_width = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
+                actual_height = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
+
+                if (actual_width, actual_height) == (width, height):
+                    supported_res.append((width, height))
+
+            cap.release()
+
+            if supported_res:
+                camera_data.append({
+                    'index': index,
+                    'name': name,
+                    'resolutions': supported_res
+                })
+    return camera_data
+"""
 
 # --- Kamera-Setup ---
 def setup_camera(index, width, height):
