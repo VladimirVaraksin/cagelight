6e016bc2559fd7c17d87d46016bd30d8b4d6651b
vlavar <145839850+VladimirVaraksin@users.noreply.github.com>
2025-05-05 21:13:46 +0200

    update

--- main.py
+++ main.py
@@ -4,21 +4,30 @@ import time
 import cv2
 import os
 import json
-
-DAUER_SPIEL = 5400  # Dauer des Spiels in Sekunden (90 Minuten)
-FPS = 30  # Frames pro Sekunde
-HALBZEIT_DAUER = 900  # Dauer der Halbzeitpause in Sekunden (15 Minuten)
-RESOLUTION = (1280, 720)  # Auflösung der Kamera
-OUTPUT_FOLDER = 'output'  # Ordner für die Ausgabedateien
+import argparse
 
 
-def main():
-    save_folder = os.path.join(OUTPUT_FOLDER, 'output_video')
+def main(lcl_args=None):
+    # Standard Konfiguration
+    dauer_spiel = 5400  # Dauer des Spiels in Sekunden (90 Minuten)
+    fps = 30  # Frames pro Sekunde
+    halbzeit_dauer = 900  # Dauer der Halbzeitpause in Sekunden (15 Minuten)
+    resolution = (1280, 720)  # Auflösung der Kamera
+    save_folder = 'output'  # Ordner für die Ausgabedateien
+    camera_id = kameranummer = 0  # Standard Kamera-ID
+
+    if lcl_args is not None:
+        dauer_spiel = lcl_args.spieldauer*60 if lcl_args.spieldauer else dauer_spiel
+        halbzeit_dauer = lcl_args.halbzeitdauer*60 if lcl_args.halbzeitdauer else halbzeit_dauer
+        fps = lcl_args.fps if lcl_args.fps else fps
+        resolution = tuple(lcl_args.resolution) if lcl_args.resolution else resolution
+        kameranummer = lcl_args.kameranummer if lcl_args.kameranummer else 0
+
     os.makedirs(save_folder, exist_ok=True)
     data = []
-    camera_id = 0
+
     frame_num = 1
-    camera = setup_camera(camera_id, RESOLUTION[0], RESOLUTION[1])
+    camera = setup_camera(camera_id, resolution[0], resolution[1])
 
     if not camera:
         print(f"Failed to open camera with ID {camera_id}.")
@@ -27,8 +36,8 @@ def main():
     out = cv2.VideoWriter(
         os.path.join(save_folder, 'output_video.mp4'),
         cv2.VideoWriter_fourcc(*'mp4v'),
-        FPS,
-        (RESOLUTION[0], RESOLUTION[1])  # Create a video writer object
+        fps,
+        (resolution[0], resolution[1])  # Create a video writer object
     )
 
     while True:
@@ -39,11 +48,11 @@ def main():
 
         frame_num += 1  # Framezähler erhöhen
 
-        if frame_num == (DAUER_SPIEL / 2) * FPS:
+        if frame_num == (dauer_spiel / 2) * fps:
             print("Halbzeit")
-            time.sleep(HALBZEIT_DAUER) # 15 Minuten Pause
+            time.sleep(halbzeit_dauer) # 15 Minuten Pause
 
-        if frame_num == DAUER_SPIEL* FPS + HALBZEIT_DAUER* FPS:
+        if frame_num == dauer_spiel* fps + halbzeit_dauer* fps:
             print("Ende des Spiels")
             break
 
@@ -64,15 +73,51 @@ def main():
             tracker='bytetrack/bytetrack_ball.yaml',
             classes=[0]
         )
-        data.append(save_objects(players, frame, frame_num))
-        data.append(save_objects(ball, frame, frame_num))
+        data.append(save_objects(players, frame, frame_num, kameranummer))
+        data.append(save_objects(ball, frame, frame_num, kameranummer))
         # Write the frame to the video file
         out.write(frame)
-        break
+
     release_sources({camera, out})
     with open(os.path.join(save_folder, 'live_output.json'), 'w') as jf:
         json.dump(data, jf, indent=4)
 
 
 if __name__ == "__main__":
-    main()
+    parser = argparse.ArgumentParser(description="Konfiguriere die Spielaufnahme.")
+
+    parser.add_argument(
+        "--spieldauer",
+        type=int,
+        help="Gesamtdauer des Spiels in Minuten."
+    )
+
+    parser.add_argument(
+        "--halbzeitdauer",
+        type=int,
+        help="Dauer der Halbzeitpause in Minuten."
+    )
+
+    parser.add_argument(
+        "--fps",
+        type=int,
+        help="Anzahl der Bilder pro Sekunde (Frames per Second) für die Aufnahme."
+    )
+
+    parser.add_argument(
+        "--resolution",
+        type=int,
+        nargs=2,
+        metavar=('BREITE', 'HÖHE'),
+        help="Auflösung der Videoaufnahme im Format: BREITE HÖHE (z. B. 1280 720)."
+    )
+
+    parser.add_argument(
+        "--kameranummer",
+        type=int,
+        help="Nummer der verwendeten Kamera z.B 0-3)."
+    )
+
+    args = parser.parse_args()
+    print(args)
+    main(args)
