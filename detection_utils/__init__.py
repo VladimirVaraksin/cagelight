8b3059372814812d2a370b4701f719a46fe1d123
vlavar <145839850+VladimirVaraksin@users.noreply.github.com>
2025-05-25 17:51:37 +0200

    update homography

--- detection_utils/__init__.py
+++ detection_utils/__init__.py
@@ -1,2 +1,2 @@
-from object_detection import annotate_objects, PLAYER_DETECTION_MODEL, BALL_MODEL
-from team_assigner import TeamAssigner
+from .object_detection import annotate_objects, PLAYER_DETECTION_MODEL, BALL_MODEL
+from .team_assigner import TeamAssigner
