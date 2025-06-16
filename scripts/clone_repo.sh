c0785c36448d534723a7ef4ad1a937b5c5b8c093
vlavar <145839850+VladimirVaraksin@users.noreply.github.com>
2025-06-16 19:06:43 +0200

    update

--- scripts/clone_repo.sh
+++ scripts/clone_repo.sh
@@ -2,7 +2,7 @@ cd ~ || exit
 mkdir CageLight
 cd CageLight || exit
 git clone https://github.com/THA-CageLight/AI--Realtime.git
-python3.10 -m venv yolovenv
+python3.12 -m venv yolovenv
 source yolovenv/bin/activate
 # Install dependencies
 sudo apt update
